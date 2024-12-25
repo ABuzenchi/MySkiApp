7540ff45cca0664200a2d69634e8a374f5d9e4e0
Andreea Buzenchi <buzenchiandreea26@gmail.com>
2024-12-25 15:54:06 +0200

    Added a background image for home page

--- client/src/pages/home/home.tsx
+++ client/src/pages/home/home.tsx
@@ -1,6 +1,10 @@
+import classes from "./home.module.css"
 const Home=()=> {
   return (
-    <div>home</div>
+    <div className={classes.pageBackground}>
+        <h1>Home</h1>
+    </div>
+    
   )
 }
 export default Home;
