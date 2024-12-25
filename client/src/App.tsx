ca5fa1a983f66a500bb97d80418217616ea86e1a
Andreea Buzenchi <buzenchiandreea26@gmail.com>
2024-12-25 15:40:00 +0200

    Created the header section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,23 @@
-import { BrowserRouter,Route,Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/home";
 import Resorts from "./pages/resorts/resorts";
 import Forum from "./pages/forum/forum";
+import Header from "./components/header/header";
+import { MantineProvider } from "@mantine/core";
 
-
-const App=() =>{
+const App = () => {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/resorts" element={<Resorts/>}/>
-      <Route path="/forum" element={<Forum/>}/>
-    </Routes>
-    </BrowserRouter>
-  )
-}
+    <MantineProvider>
+      <BrowserRouter>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/resorts" element={<Resorts />} />
+          <Route path="/forum" element={<Forum />} />
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
+  );
+};
 
 export default App;
---
+++ client/src/components/header/header.tsx
@@ -0,0 +1,40 @@
+import classes from "./header.module.css";
+import alpineSkiingLight from "../../assets/alpine-skiing-light.png";
+import { Button, Image } from "@mantine/core";
+import { AiFillHome } from "react-icons/ai";
+import { FaMap } from "react-icons/fa";
+import { MdForum } from "react-icons/md";
+import { FaCircleUser } from "react-icons/fa6";
+
+const Header = () => {
+
+  return (
+    <>
+    <div className={classes.navBar}>
+      <Image src={alpineSkiingLight} />
+
+      <Button component="a" href="/" size="lg">
+        <AiFillHome />
+        Home
+      </Button>
+
+      <Button component="a" href="/resorts" size="lg">
+        <FaMap />
+        Resorts
+      </Button>
+
+      <Button component="a" href="/forum" size="lg">
+        <MdForum />
+        Forum
+      </Button>
+
+      <Button component="a" href="/user-profile" size="lg">
+        <FaCircleUser />
+        User Profile
+      </Button>
+    </div>
+    </>
+  );
+};
+
+export default Header;
