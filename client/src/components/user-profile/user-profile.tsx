1cc6df52ca1e46a70da86f6db7a6902749cea31c
Andreea Buzenchi <buzenchiandreea26@gmail.com>
2024-12-28 17:35:49 +0200

    The drawer opens over the header

--- client/src/components/user-profile/user-profile.tsx
+++ client/src/components/user-profile/user-profile.tsx
@@ -1,5 +1,6 @@
 import { Button, Drawer } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import classes from "./user-profile.module.css"
 
 const UserProfile=()=>{
   const [opened, { open, close }] = useDisclosure(false);
@@ -12,6 +13,10 @@ const UserProfile=()=>{
         title="Authentication"
         overlayProps={{ backgroundOpacity: 0.5, blur: 4 }}
         position="right"
+        classNames={{
+          root: classes.drawerRoot, // Stil personalizat pentru poziționare exactă
+          header: classes.drawerHeader, // Poți adăuga stiluri și pentru alte părți
+        }}
       >
       <div>Meow meow meow</div>
       </Drawer>
