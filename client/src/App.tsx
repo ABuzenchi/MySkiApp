62e6a4ba47448bfccad185c43c1d72debf666d2a
Andreea Buzenchi <buzenchiandreea26@gmail.com>
2025-01-15 15:43:42 +0200

    Deleted resorts page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/home";
-import Resorts from "./pages/resorts/resorts";
 import Forum from "./pages/forum/forum";
 import Header from "./components/header/header";
 import { MantineProvider } from "@mantine/core";
@@ -20,7 +19,6 @@ const App = () => {
         <Header></Header>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/resorts" element={<Resorts />} />
           <Route path="/forum" element={<Forum />} />
           <Route path="/Sinaia" element={ <Sinaia/>} />
           <Route path="/Poiana Brasov" element={ <PoianaBrasov/>} />
