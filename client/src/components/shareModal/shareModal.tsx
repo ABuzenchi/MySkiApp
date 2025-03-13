f81d2d5d9f817c36373b77f575a7b97b9c939173
Andreea Buzenchi <buzenchiandreea26@gmail.com>
2025-03-13 22:49:26 +0200

    Added facebook and whatsapp icons in shareModal

--- client/src/components/shareModal/shareModal.tsx
+++ client/src/components/shareModal/shareModal.tsx
@@ -1,20 +1,40 @@
-import { useDisclosure } from '@mantine/hooks';
-import { Modal, Button } from '@mantine/core';
-import { FaShareAlt } from 'react-icons/fa';
+import { useDisclosure } from "@mantine/hooks";
+import { Modal, Button } from "@mantine/core";
+import { FaShareAlt } from "react-icons/fa";
+import classes from "./shareModal.module.css";
+import { FaWhatsapp } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa6";
 
-const ShareModal=()=> {
+const ShareModal = () => {
   const [opened, { open, close }] = useDisclosure(false);
 
   return (
     <>
-      <Modal opened={opened} onClose={close} title="Share" centered={true}  overlayProps={{ backgroundOpacity: 0.5, blur: 4 }}>
-       Meow
+      <Modal
+        opened={opened}
+        onClose={close}
+        title="Share"
+        centered
+        overlayProps={{ backgroundOpacity: 0.5, blur: 4 }}
+        size='md'
+        classNames={{
+          title: classes.customModalTitle, 
+        }}
+      >
+        <div className={classes.container}>
+          <p>QR CODE</p>
+          <Button variant="transparent" size="xl" className={classes.whatsappButton}>
+            <FaWhatsapp />
+          </Button>
+          <Button variant="transparent" size="xl" className={classes.facebookButton}>
+            <FaFacebook />
+          </Button>
+        </div>
       </Modal>
-
       <Button variant="transparent" size="sm" onClick={open}>
         <FaShareAlt />
       </Button>
     </>
   );
-}
+};
 export default ShareModal;
