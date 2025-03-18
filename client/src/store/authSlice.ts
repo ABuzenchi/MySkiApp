61c0f0660a4d75947b56a8bafc434c3c1419e335
Andreea Buzenchi <buzenchiandreea26@gmail.com>
2025-03-18 10:20:06 +0200

    Added favoriteSlopes, removeSlope and visitedSlope in  authSlice

--- client/src/store/authSlice.ts
+++ client/src/store/authSlice.ts
@@ -2,13 +2,17 @@ import { createSlice } from "@reduxjs/toolkit";
 interface AuthState {
   username: string | null;
   isAuthenticated: boolean;
-  avatar:string| null;
+  avatar: string | null;
+  favoriteSlopes: string[];
+  visitedSlopes: string[];
 }
 
 const initialState: AuthState = {
   username: null,
   isAuthenticated: false,
-  avatar:null
+  avatar: null,
+  favoriteSlopes:[],
+  visitedSlopes:[],
 };
 
 const authSlice = createSlice({
@@ -18,18 +22,28 @@ const authSlice = createSlice({
     login(state, action) {
       state.username = action.payload.username;
       state.isAuthenticated = true;
-      state.avatar = action.payload.avatar || null; 
+      state.avatar = action.payload.avatar || null;
     },
     logout(state) {
       state.username = null;
       state.isAuthenticated = false;
-      state.avatar=null;
+      state.avatar = null;
     },
     setAvatar(state, action) {
-      state.avatar = action.payload; 
+      state.avatar = action.payload;
     },
+    addFavoriteSlopes(state,action){
+      state.favoriteSlopes.push(action.payload);
+    },
+    addVisitedSlopes(state, action){
+      state.visitedSlopes.push(action.payload);
+    },
+    removeFavoriteSlope(state,action){
+      state.favoriteSlopes=state.favoriteSlopes.filter((slope)=>slope!==action.payload);
+    }
+    
   },
 });
 
-export const { login, logout, setAvatar } = authSlice.actions;
+export const { login, logout, setAvatar,addFavoriteSlopes,addVisitedSlopes,removeFavoriteSlope } = authSlice.actions;
 export default authSlice.reducer;
